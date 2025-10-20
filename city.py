7037cecd59dada945b94425ea17d1860930e5b0f
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-10-19 23:18:37 -0300

    Ajusta city.py

--- city.py
+++ city.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
 """
 Classe City - Representa uma cidade com seu mapa bidimensional
-Utiliza apenas arrays clássicos (acesso por índice)
 """
 
 import json
 
 class City:
-    """Representa uma cidade com mapa bidimensional usando arrays clássicos"""
-    
-    # Tipos de células
+
     EMPTY = 0
     STREET = 1
     HOUSE = 2
     BUILDING = 3
     PARK = 4
-    TRAFFIC = 5  # Engarrafamento
+    TRAFFIC = 5  
     
-    # Nomes dos tipos
     CELL_NAMES = {
         0: "Vazio",
         1: "Rua",
@@ -28,18 +24,10 @@ class City:
     }
     
     def __init__(self, width, height, name="Cidade"):
-        """
-        Inicializa uma cidade
-        :param width: Largura do mapa (número de colunas)
-        :param height: Altura do mapa (número de linhas)
-        :param name: Nome da cidade
-        """
         self.width = width
         self.height = height
         self.name = name
         
-        # Array bidimensional clássico - implementado como array de arrays
-        # Apenas acesso por índice é permitido
         self.grid = []
         for i in range(height):
             row = []
@@ -47,11 +35,9 @@ class City:
                 row.append(self.EMPTY)
             self.grid.append(row)
         
-        # Arrays para armazenar posições de tráfego
         self.traffic_positions = []
     
     def set_cell(self, x, y, cell_type):
-        """Define o tipo de uma célula"""
         if 0 <= y < self.height and 0 <= x < self.width:
             self.grid[y][x] = cell_type
             return True
@@ -74,7 +60,7 @@ class City:
         """Adiciona engarrafamento em uma posição"""
         if self.get_cell(x, y) == self.STREET:
             self.set_cell(x, y, self.TRAFFIC)
-            # Adiciona à lista de tráfego
+
             pos = [x, y]
             self.traffic_positions.append(pos)
             return True
@@ -84,12 +70,10 @@ class City:
         """Remove engarrafamento de uma posição"""
         if self.get_cell(x, y) == self.TRAFFIC:
             self.set_cell(x, y, self.STREET)
-            # Remove da lista de tráfego
             i = 0
             while i < len(self.traffic_positions):
                 pos = self.traffic_positions[i]
                 if pos[0] == x and pos[1] == y:
-                    # Remove manualmente sem usar métodos avançados
                     new_traffic = []
                     for j in range(len(self.traffic_positions)):
                         if j != i:
@@ -102,18 +86,15 @@ class City:
     
     def clear_all_traffic(self):
         """Remove todos os engarrafamentos"""
-        # Percorre array de tráfego e limpa
         for i in range(len(self.traffic_positions)):
             pos = self.traffic_positions[i]
             x, y = pos[0], pos[1]
             if self.get_cell(x, y) == self.TRAFFIC:
                 self.set_cell(x, y, self.STREET)
         
-        # Limpa array de tráfego
         self.traffic_positions = []
     
     def generate_random_traffic(self, num_traffic):
-        """Gera engarrafamentos aleatórios em ruas"""
         import random
         
         # Primeiro limpa tráfego existente
@@ -126,7 +107,6 @@ class City:
                 if self.grid[y][x] == self.STREET:
                     street_positions.append([x, y])
         
-        # Adiciona tráfego aleatório
         count = 0
         attempts = 0
         max_attempts = num_traffic * 10
@@ -150,7 +130,6 @@ class City:
         self.clear_all_traffic()
     
     def save_to_file(self, filename):
-        """Salva o mapa em arquivo JSON"""
         data = {
             'name': self.name,
             'width': self.width,
